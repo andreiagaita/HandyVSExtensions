a3deca8f28b9fc55681db1387984e7a5b8f24ad7
Andreia Gaita <shana@spoiledcat.net>
2014-12-14 15:47:10 +0100

    Update descriptions

--- Utils/Properties/AssemblyInfo.cs
+++ Utils/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Utils")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SpoiledCat.Utils")]
+[assembly: AssemblyDescription("Utility code")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Utils")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Andreia Gaita 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
